c482f648330fc27938c38f6bdfdd22a0dba2d89d
yyf1074366085 <yyf1074366085@gmail.com>
2024-12-22 23:08:42 +0800

    create project

---
+++ src/main/java/com/dlxz/profchoice/entity/User.java
@@ -0,0 +1,15 @@
+package com.dlxz.profchoice.entity;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private enum role{
+
+    }
+    private String department;
+    private boolean status;
+    private
+}
